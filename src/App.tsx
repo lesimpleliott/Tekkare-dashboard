f7b6bbbf70960bbf02eeb143ed935e949216dde4
Eliott Lesimple <lesimpleliott@gmail.com>
2024-09-06 20:58:16 +0200

    Create CTA // i18Next switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { useTranslation } from "react-i18next";
 
 const languages = [
-  { id: "fr", name: "Français", flag: "./fr.svg" },
-  { id: "en", name: "English", flag: "./en.svg" },
+  { id: "fr", name: "Français", flag: "./flags/fr.svg" },
+  { id: "en", name: "English", flag: "./flags/en.svg" },
 ];
 
 const App = () => {
@@ -12,16 +12,19 @@ const App = () => {
     i18n.changeLanguage(lang);
   };
 
+  const root = document.getElementById("root");
+  root!.className = "flex min-h-dvh relative";
+
   return (
-    <section className="flex flex-col p-6">
-      <h1 className="w-fit bg-gradient-to-r from-primary to-secondary bg-clip-text text-6xl font-bold text-transparent">
+    <section className="flex w-full flex-col p-6">
+      <h1 className="w-fit bg-gradient-to-r from-primary to-secondary bg-clip-text text-xl font-bold text-transparent">
         {t("welcomeMessage")}
       </h1>
 
-      <div className="mt-4 flex gap-2">
+      <div className="mt-4 flex flex-col gap-2">
         {languages.map((lang) => (
           <button
-            className="cta flex items-center justify-center gap-2"
+            className="cta shadow-cta-blue flex w-fit min-w-[150px] items-center justify-center gap-2 px-6 py-2 text-white"
             key={lang.id}
             onClick={() => handleLanguageChange(lang.id)}
           >
