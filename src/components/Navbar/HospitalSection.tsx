a2944d66ef4d2f32f0473733a7d4f90fca124d41
Eliott Lesimple <lesimpleliott@gmail.com>
2024-09-07 20:26:28 +0200

    Delete the hospital icon on top of navbar

--- src/components/Navbar/HospitalSection.tsx
+++ src/components/Navbar/HospitalSection.tsx
@@ -1,4 +1,3 @@
-import { NavLink } from "react-router-dom";
 import { useHospitalStore } from "../../stores/datas.store";
 import HospitalMenu from "./HospitalMenuTitle";
 
@@ -7,9 +6,9 @@ const Hospitals = () => {
 
   return (
     <section className="flex flex-1 flex-col items-start border-y-2 border-slate-200">
-      <NavLink to="/">
+      {/* <NavLink to="/">
         <i className="fa-solid fa-hospital mt-4 flex h-10 min-w-10 items-center justify-center rounded-full px-3 text-2xl text-gray-500"></i>
-      </NavLink>
+      </NavLink> */}
       {hospitals.map((hospital) => HospitalMenu({ hospital }))}
     </section>
   );
