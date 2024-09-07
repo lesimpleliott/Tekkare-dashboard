9cdcbc47894d6785685c0678071761ce161ef041
Eliott Lesimple <lesimpleliott@gmail.com>
2024-09-07 19:29:47 +0200

    Reorganize and Add Tailwind Color

--- src/components/MainTitle.tsx
+++ src/components/MainTitle.tsx
@@ -1,7 +1,7 @@
 const MainTitle = ({ title }: { title: string }) => {
   return (
     <section className="mb-6 flex justify-end">
-      <h1 className="w-fit bg-gradient-to-r from-primary to-secondary bg-clip-text text-right text-3xl font-bold text-transparent">
+      <h1 className="w-fit bg-gradient-to-r from-primary-500 to-secondary-500 bg-clip-text text-right text-3xl font-bold text-transparent">
         {title}
       </h1>
     </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,43 @@ export default {
         custom: ["Figtree", "sans-serif"],
       },
       colors: {
-        background: "#2100ad",
-        primary: "#5dc8ff",
-        secondary: "#F962E5",
+        main: {
+          100: "#e0d7ff",
+          200: "#b5a9ff",
+          300: "#8a7dff",
+          400: "#5d52ff",
+          500: "#2100ad", //default
+          600: "#1a0096",
+          700: "#14007d",
+          800: "#0e005c",
+          900: "#07003a",
+        },
+        primary: {
+          100: "#d0f2ff",
+          200: "#a0e0ff",
+          300: "#70cfff",
+          400: "#40bfff",
+          500: "#5dc8ff", //default
+          600: "#49a6e0",
+          700: "#3483b6",
+          800: "#206084",
+          900: "#103053",
+        },
+        secondary: {
+          100: "#fcd7f5",
+          200: "#f9aaf2",
+          300: "#f77ce8",
+          400: "#f54fe4",
+          500: "#F962E5", //default
+          600: "#d852c2",
+          700: "#b944a0",
+          800: "#9e367d",
+          900: "#6e214e",
+        },
       },
       boxShadow: {
-        "cta-blue": "inset 2px 1000px 1px #2100ad",
-        "cta-gray": "inset 2px 1000px 1px rgb(229 231 235)",
+        "cta-blue": "inset 2px 1000px 1px #2100ad", //background-500
+        "cta-gray": "inset 2px 1000px 1px rgb(229 231 235)", //gray-200
       },
     },
   },
