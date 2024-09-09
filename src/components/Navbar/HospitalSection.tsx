1b447df0d972264e502c7c724fff542789a5caa7
Eliott Lesimple <lesimpleliott@gmail.com>
2024-09-09 12:51:25 +0200

    NavBar // Add Hospitals icon

--- src/components/Navbar/HospitalSection.tsx
+++ src/components/Navbar/HospitalSection.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import { useHospitalStore } from "../../stores/datas.store";
 import HospitalMenu from "./HospitalMenuTitle";
 
@@ -5,10 +6,10 @@ const Hospitals = () => {
   const hospitals = useHospitalStore((state) => state.hospitals);
 
   return (
-    <section className="flex flex-1 flex-col items-start border-y-2 border-slate-200">
-      {/* <NavLink to="/">
-        <i className="fa-solid fa-hospital mt-4 flex h-10 min-w-10 items-center justify-center rounded-full px-3 text-2xl text-gray-500"></i>
-      </NavLink> */}
+    <section className="flex flex-1 flex-col items-start border-y-2 border-slate-200 pt-6">
+      <NavLink to="/" className="hospitalLink relative">
+        <i className="fa-solid fa-hospital h-10 w-10 min-w-10 flex items-center justify-start rounded-full px-3 text-2xl text-gray-500"></i>
+      </NavLink>
       {hospitals.map((hospital) => HospitalMenu({ hospital }))}
     </section>
   );
