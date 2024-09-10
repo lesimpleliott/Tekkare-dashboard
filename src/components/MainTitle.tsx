ec76e8e11542c07d78c4df405b937aea2479df53
Eliott Lesimple <lesimpleliott@gmail.com>
2024-09-10 10:56:21 +0200

    Title // Improve style

--- src/components/MainTitle.tsx
+++ src/components/MainTitle.tsx
@@ -5,11 +5,13 @@ type MainTitleProps = {
 
 const MainTitle = ({ title, subtitle }: MainTitleProps) => {
   return (
-    <section className="mb-6 ml-14 flex justify-end">
-      <h1 className="from-primary-500 to-secondary-500 w-fit bg-gradient-to-r bg-clip-text text-right text-3xl font-bold text-transparent">
-        <span className="">{title}</span>
+    <section className="mb-6 ml-14 flex justify-end lg:ml-0 lg:justify-center">
+      <h1 className="text-right lg:text-center">
+        <span className="w-fit bg-gradient-to-r from-primary-500 to-secondary-500 bg-clip-text text-3xl font-bold text-transparent lg:text-4xl">
+          {title}
+        </span>
         {subtitle && (
-          <span className="block text-lg font-medium text-gray-50">
+          <span className="block text-lg font-medium text-gray-50 lg:text-xl">
             {subtitle}
           </span>
         )}
