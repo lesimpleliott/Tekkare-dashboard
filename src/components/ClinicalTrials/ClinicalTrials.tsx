ce4a569972796ad7e7ffe2e9ab7f385206d8248f
Eliott Lesimple <lesimpleliott@gmail.com>
2024-09-12 14:49:21 +0200

    Clinical Trials // Add hospital filter to Home page

--- src/components/ClinicalTrials/ClinicalTrials.tsx
+++ src/components/ClinicalTrials/ClinicalTrials.tsx
@@ -13,53 +13,85 @@ const ClinicalTrials = ({
   hospitalDisplay?: boolean;
 }) => {
   const { t, i18n } = useTranslation();
-  const [filter, setFilter] = useState("all");
+  const [filterStatut, setFilterStatut] = useState("all");
+  const [filterHospital, setFilterHospital] = useState("all");
   const hospitals = useHospitalStore((state) => state.hospitals);
 
   // Tableau des boutons de filtre
   const filterButtons = [
     { label: "all", text: "status.all" },
-    { label: "completed", text: "status.completed" },
     { label: "inProgress", text: "status.inProgress" },
+    { label: "completed", text: "status.completed" },
   ];
 
-  // Fonction pour obtenir les essais cliniques filtrés et triés
-  const filteredAndSortedTrials = trials
-    .filter((trial) => {
-      const progress = calculateProgress(trial.startDate, trial.endDate);
-      const isCompleted = progress >= 100;
+  const filteredAndSortedTrials = trials.filter((trial) => {
+    const progress = calculateProgress(trial.startDate, trial.endDate);
+    const isCompleted = progress >= 100;
 
-      if (filter === "completed") {
-        return isCompleted;
-      } else if (filter === "inProgress") {
-        return !isCompleted;
-      } else {
-        return true;
-      }
-    })
-    .sort(
-      (a, b) => new Date(b.endDate).getTime() - new Date(a.endDate).getTime(),
-    );
+    // Filtre par statut
+    if (filterStatut === "completed" && !isCompleted) {
+      return false;
+    } else if (filterStatut === "inProgress" && isCompleted) {
+      return false;
+    }
+
+    // Filtre par hôpital
+    if (filterHospital !== "all") {
+      const hospital = hospitals.find((hospital) =>
+        hospital.clinicalTrials.some(
+          (hTrial) => hTrial.trialId === trial.trialId,
+        ),
+      );
+      return hospital && hospital.id === filterHospital;
+    }
+
+    return true;
+  });
+  // // Tri par date de fin
+  // .sort(
+  //   (a, b) => new Date(b.endDate).getTime() - new Date(a.endDate).getTime(),
+  // );
 
   return (
-    <section className="mt-4">
-      <div className="mt-6 flex flex-col items-start justify-center gap-x-4 gap-y-2 pb-4 sm:flex-row sm:justify-between">
+    <section className="mt-4 space-y-4">
+      <div className="mt-6 flex flex-col items-start justify-center gap-x-4 gap-y-2 sm:flex-row sm:justify-between">
         {/* Titre */}
         <h2>{t("clinicalTrials")}</h2>
 
-        {/* Filter Buttons */}
-        <div className="flex justify-end gap-2">
+        {/* Filter Progress Statut */}
+        <div className="flex flex-wrap justify-end gap-2">
           {filterButtons.map((button, index) => (
             <button
               key={index}
-              className={`filterButton ${filter === button.label ? "active" : ""}`}
-              onClick={() => setFilter(button.label)}
+              className={`filterButton ${filterStatut === button.label ? "active" : ""}`}
+              onClick={() => setFilterStatut(button.label)}
             >
               {t(button.text)}
             </button>
           ))}
         </div>
       </div>
+
+      {/* Filter Hospital */}
+      {hospitalDisplay && (
+        <div className="flex flex-wrap gap-2 border-t border-main-200 pt-4 sm:justify-center">
+          <button
+            className={`filterButton ${filterHospital === "all" ? "active" : ""}`}
+            onClick={() => setFilterHospital("all")}
+          >
+            {t("status.all")}
+          </button>
+          {hospitals.map((hospital) => (
+            <button
+              key={hospital.id}
+              className={`filterButton ${filterHospital === hospital.id ? "active" : ""}`}
+              onClick={() => setFilterHospital(hospital.id)}
+            >
+              {t(hospital.name[i18n.language as keyof typeof hospital.name])}
+            </button>
+          ))}
+        </div>
+      )}
 
       <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
         {filteredAndSortedTrials.map((trial, index) => {
--- src/components/ClinicalTrials/TrialCard.tsx
+++ src/components/ClinicalTrials/TrialCard.tsx
@@ -15,56 +15,59 @@ const TrialCard = ({
 
   const progress = calculateProgress(trial.startDate, trial.endDate);
   const isCompleted = progress >= 100;
-  const [cardIsOpen, setCardIsOpen] = useState(true); // Toggle Info 'false' par défaut
+  const [cardIsOpen, setCardIsOpen] = useState(false); // Toggle Info 'false' par défaut
   const capitalize = (str: string) =>
     str.charAt(0).toUpperCase() + str.slice(1);
 
   return (
     <article className="KPICard flex h-fit flex-col gap-2 px-4 pt-4">
-      <div className="items-between flex justify-between gap-2">
-        {/* Titre essai */}
-        <h3 className="w-full pr-10 text-lg font-bold leading-5">
-          {String(trial.name[i18n.language as keyof typeof trial.name])}
-        </h3>
+      {/* CARD // VISIBLE DATA */}
+      <section className="flex flex-col justify-between">
+        <div className="items-between flex justify-between gap-2">
+          {/* Titre essai */}
+          <h3 className="pr-10 text-lg font-bold leading-5 lg:line-clamp-3 lg:h-16">
+            {String(trial.name[i18n.language as keyof typeof trial.name])}
+          </h3>
 
-        {/* Toggle Info button */}
-        <button
-          onClick={() => setCardIsOpen(!cardIsOpen)}
-          className="relative h-6 min-w-6 rounded-full border-2 border-gray-400 bg-gray-200 text-sm text-gray-400"
-        >
-          <i className="fa-solid fa-info absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"></i>
-        </button>
+          {/* Toggle Info button */}
+          <button
+            onClick={() => setCardIsOpen(!cardIsOpen)}
+            className="relative h-6 min-w-6 rounded-full border-2 border-gray-400 bg-gray-200 text-sm text-gray-400"
+          >
+            <i className="fa-solid fa-info absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"></i>
+          </button>
 
-        {/* TAG */}
-        <p
-          className={`h-6 text-nowrap rounded-full border-2 px-2 text-sm font-semibold ${
-            isCompleted
-              ? "border-primary-500 bg-primary-100 text-primary-500"
-              : "border-secondary-500 bg-secondary-100 text-secondary-500"
-          }`}
-        >
-          {isCompleted ? t("status.completed") : t("status.inProgress")}
-        </p>
-      </div>
-
-      {/* Barre de progression */}
-      <div className="flex items-center justify-center gap-2">
-        <div className="relative h-3 w-full rounded-full bg-main-100">
-          <span
-            className={`absolute h-full rounded-full transition-all duration-500 ${
-              isCompleted ? "w-full bg-primary-500" : "bg-secondary-500"
+          {/* TAG */}
+          <p
+            className={`h-6 text-nowrap rounded-full border-2 px-2 text-sm font-semibold ${
+              isCompleted
+                ? "border-primary-500 bg-primary-100 text-primary-500"
+                : "border-secondary-500 bg-secondary-100 text-secondary-500"
             }`}
-            style={{
-              width: isCompleted ? "100%" : `${Math.min(progress, 100)}%`,
-            }}
-          ></span>
+          >
+            {isCompleted ? t("status.completed") : t("status.inProgress")}
+          </p>
         </div>
-        <p className="font-medium">{Math.min(progress, 100)}%</p>
-      </div>
 
-      {/* Infos */}
+        {/* Barre de progression */}
+        <div className="flex items-center justify-center gap-2">
+          <div className="relative h-3 w-full rounded-full bg-main-100">
+            <span
+              className={`absolute h-full rounded-full transition-all duration-500 ${
+                isCompleted ? "w-full bg-primary-500" : "bg-secondary-500"
+              }`}
+              style={{
+                width: isCompleted ? "100%" : `${Math.min(progress, 100)}%`,
+              }}
+            ></span>
+          </div>
+          <p className="font-medium">{Math.min(progress, 100)}%</p>
+        </div>
+      </section>
+
+      {/* CARD // COLLAPSED DATA */}
       <section
-        className={`flex flex-col overflow-hidden text-sm transition-all duration-300 ease-out ${cardIsOpen ? "h-fit border-t border-main-200 pb-3 pt-2" : "h-0"}`}
+        className={`flex flex-col overflow-hidden text-sm transition-all duration-500 ease-out ${cardIsOpen ? "h-fit border-t border-main-200 pb-3 pt-2" : "h-0"}`}
       >
         {/* Trial ID */}
         <div className="">
